9955b5abf76c68fd4ddbb887ff1bc18a4a0d04da
Nitin Madhukar <iec2018053@iiita.ac.in>
2019-10-12 00:34:29 +0530

    3d-models

---
+++ shoes/Microsoft VS Code/_/resources/app/out/vs/workbench/contrib/debug/node/telemetryApp.nls.js
@@ -0,0 +1,5 @@
+/*!--------------------------------------------------------
+ * Copyright (C) Microsoft Corporation. All rights reserved.
+ *--------------------------------------------------------*/
+define("vs/workbench/contrib/debug/node/telemetryApp.nls",{"vs/base/node/processes":["Can't execute a shell command on a UNC drive."],"vs/platform/configuration/common/configurationRegistry":["Default Configuration Overrides","Configure editor settings to be overridden for {0} language.","Configure editor settings to be overridden for a language.","Cannot register '{0}'. This matches property pattern '\\\\[.*\\\\]$' for describing language specific editor settings. Use 'configurationDefaults' contribution.","Cannot register '{0}'. This property is already registered."]});
+//# sourceMappingURL=https://ticino.blob.core.windows.net/sourcemaps/88f15d17dca836346e787762685a40bb5cce75a8/core/vs/workbench/contrib/debug/node/telemetryApp.nls.js.map
